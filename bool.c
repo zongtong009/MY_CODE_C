175cb8890815ed73ee1d143c8a98e4452070c2b1
zongtong009 <zongtongguilai1@gmail.com>
2018-11-20 23:34:55 +0800

    d1120

--- bool.c
+++ bool.c
@@ -6,18 +6,18 @@ typedef enum {
 int main(void) {
 	int n = 10;    //计算叠乘数
 	int sum = 1;   //用来存放叠乘的结果
-	bool flag = true;    //叠乘标记
+	bool flag = false;    //叠乘标记
 
 	int num = n;    //循环次数
-	while( flag ) {
+	while( !flag ) {
 		sum = sum * (num--);//当num=1时结束循环
 		printf ("叠乘到%d的叠乘值为 %d \n", num+1, sum);
 		if( num == 0) {
-			flag = false;
+			flag = true;
 		}
 	}
 	printf ("%d的叠乘值为 %d \n", n, sum);
-	
+
 	return 0;
 }
 
---
+++ test002.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<stdbool.h>
+/****************************************************************/
+int main(int argc, char *argv[]) {
+	int t=0;
+	while(true){
+		t++;
+		printf("wtfrgfe\n");
+		if(t>100000){
+			break;
+		}
+	}
+
+	return 0;
+}
+
