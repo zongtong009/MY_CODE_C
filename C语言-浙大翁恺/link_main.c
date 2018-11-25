27984d22074949f5da00e10abcb5028655e45f9a
zongtong009 <zongtongguilai1@gmail.com>
2018-11-26 03:00:59 +0800

    Update link_main.c

--- C语言-浙大翁恺/link_main.c
+++ C语言-浙大翁恺/link_main.c
@@ -1,41 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include"link.h"
+#include "link.h"
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-typedef struct _list{
-	Node *head;
-}List;
+typedef struct _list
+{
+    Node *head;
+} List;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-	Node *head=NULL;
-	List list;
-	int number;
-	list.head=NULL;
-	do{
-		scanf("%d",&number);
-		if (number!=-1){
-			head=add(head,number)	
-		}
-	}while (number!=-1);
+    Node *head = NULL;
+    List list;
+    int number;
+    list.head = NULL;
+    do
+    {
+        scanf("%d", &number);
+        if (number != -1)
+        {
+            head = add(head, number)
+        }
+    } while (number != -1);
 
-
-	return 0;
+    return 0;
 }
-void add(List *pList,int number){
-	//add to linked-list
-	Node *p=(Node*)malloc(sizeof(Node));
-	p->value=number;
-	p->next=NULL;
-	//find the last 
-	Node *last=pList->head;
-	if(last){
-		while(last->next){
-			last=last->next
-		}
-		// attach
-		last->next=p;
-	}else{
-		pList->head=p; 
-	}
+void add(List *pList, int number)
+{
+    //add to linked-list
+    Node *p = (Node *)malloc(sizeof(Node));
+    p->value = number;
+    p->next = NULL;
+    //find the last
+    Node *last = pList->head;
+    if (last)
+    {
+        while (last->next)
+        {
+            last = last->next
+        }
+        // attach
+        last->next = p;
+    }
+    else
+    {
+        pList->head = p;
+    }
+}
+int funnn(void){
+   printf()
+   
+
 }
